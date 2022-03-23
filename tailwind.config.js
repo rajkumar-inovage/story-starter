75c7b86f4a0dc44ed9abbba44ad91518de97c415
Sameer Haque <50763437+rajkumar-inovage@users.noreply.github.com>
2022-03-23 22:04:10 +0530

    Signed-off-by: Sameer Haque <50763437+rajkumar-inovage@users.noreply.github.com>

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+  purge: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
+  theme: {
+    fontFamily: {
+      'sans': 'Roboto, Arial, sans-serif',
+      'serif': 'Merriweather, Georgia, serif'
+    },
+    extend: {
+      colors: {
+        'primary': '#672E9B',
+        gray: {
+          300: '#f0f0f0',
+        }
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
